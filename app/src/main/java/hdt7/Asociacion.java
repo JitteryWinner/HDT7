96eafdef09f75a8422f5a6e6c30cf0cc9876a391
JitteryWinner <davidaberganza@gmail.com>
2026-03-20 08:35:05 -0600

    Se eliminaron las clases de la base de la plantilla y se agrego la primera clase Asociacion.java

---
+++ app/src/main/java/hdt7/Asociacion.java
@@ -0,0 +1,39 @@
+/**
+ * Clase Asociacion<K, V>
+ * Representa una relación clave-valor.
+ * 
+ * Referencia:
+ * Basado en Map.Entry de Java
+ */
+public class Asociacion<K extends Comparable<K>, V> implements Comparable<Asociacion<K, V>> {
+
+    private K clave;
+    private V valor;
+
+    public Asociacion(K clave, V valor) {
+        this.clave = clave;
+        this.valor = valor;
+    }
+
+    public K getClave() {
+        return clave;
+    }
+
+    public V getValor() {
+        return valor;
+    }
+
+    public void setValor(V valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public int compareTo(Asociacion<K, V> otra) {
+        return this.clave.compareTo(otra.clave);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + clave + ", " + valor + ")";
+    }
+}
