558f147abca5b5327831d2c9bdbc6882525e0f35
JitteryWinner <davidaberganza@gmail.com>
2026-03-20 08:36:36 -0600

    Se agrego la clase Nodo

---
+++ app/src/main/java/hdt7/Nodo.java
@@ -0,0 +1,15 @@
+/**
+ * Nodo para el árbol binario
+ */
+public class Nodo<E> {
+
+    E dato;
+    Nodo<E> izquierdo;
+    Nodo<E> derecho;
+
+    public Nodo(E dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
